8711ced94826dbe3062eada705c834f663bf3f7f
alisahartle <54589015+alisahartle@users.noreply.github.com>
2019-10-17 19:31:26 -0400

    Assigned arrests to ntas using over and plotted in Rstudio

---
+++ test_script.R
@@ -0,0 +1,42 @@
+library(sp)
+
+xy <- arrests[,c("longitude", "latitude")]
+
+spdf <- SpatialPointsDataFrame(coords = xy, data = arrests,
+                               proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+
+
+
+idk <- over(n_tabs, spdf, returnList = TRUE)
+
+idk_new <- list()
+for (i in 1:195) {
+  idk_new[i] <- nrow(idk[[i]])
+}
+
+nta_df <- data.frame(n_tabs$ntacode)
+arrests_nta <- data.frame(matrix(unlist(idk_new), nrow=length(idk_new), byrow=T))
+names(arrests_nta) <- c("counts")
+row.names(arrests_nta) <- n_tabs$ntacode
+
+
+bins <- c(0, 30000, 35000, 40000, 45000, 50000, 550000, 60000, Inf)
+merge_things2 <- merge(x = n_tabs, y = arrests_nta, 
+                      by.x = "ntacode", by.y = 0)
+
+bins <- c(0, 2, 4, 8, 10, 20, Inf)
+pal <- colorBin("OrRd", domain = merge_things2$counts, bins = bins)
+
+base <-leaflet() %>%
+  setView(lat = 40.75, lng = -74, zoom = 11.3)
+
+base <- base %>%
+  addProviderTiles(providers$Stamen.TonerBackground)
+
+base %>%
+  addPolygons(data = merge_things2, color = '#7d2218', fillColor = ~pal(counts), 
+              fillOpacity = 1, weight = 2) %>%
+  addLegend(pal = pal, values = merge_things$counts,
+            opacity = 1.0, title = 'Population Totals',
+            position = "bottomleft")
